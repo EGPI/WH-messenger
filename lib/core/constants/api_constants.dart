3d2cc4fa275779f11ce192b8d7956e66f5493d52
abdelrahman <abd.saadfahmy@gmail.com>
2026-06-07 16:30:34 +0300

    icon added and connected to live server

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://192.168.1.5:8000/api';
+  static const String baseUrl = 'https://wh-egpi.com/messenger-api/api';
 
   static const String login = '/auth/login';
   static const String register = '/auth/register';
